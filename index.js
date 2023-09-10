1904d95d84f2dac0ad1e91c694edd587e3eab8e9
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-10 16:18:19 -0700

    Quick test prompt

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 const db = mysql.createConnection(
     {
@@ -9,5 +10,25 @@ const db = mysql.createConnection(
       password: process.env.DB_PASSWORD,
       database: 'employees_db'
     },
-    console.log(`Connected to the movies_db database.`)
+    console.log(`Connected to the employees_db database.`)
 );
+
+
+inquirer.promt({
+    type: 'list',
+    message: "Choose:",
+    name: "query",
+    choices:[
+        "View all departments",
+        "View all roles",
+        "View allemployees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role"
+    ],
+    validate: (input) => {
+        return input ? true : (console.log(this.message), false)
+    }
+});
+
