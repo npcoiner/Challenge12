0a6e106b01f9e37fa5071d48e1b6eafb8b41e38c
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-10 16:05:19 -0700

    Added gitignore

---
+++ index.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // TODO: Add MySQL password here
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+);
