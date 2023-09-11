a4dd34ef1c19eddb67bcf605f685797adf063019
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-10 17:41:50 -0700

    Added .env Added prompts Added switch for user choices

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); 
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
@@ -5,6 +6,7 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
+      port: 3306,
       user:  process.env.DB_USER,
       // TODO: Add MySQL password here
       password: process.env.DB_PASSWORD,
@@ -14,21 +16,77 @@ const db = mysql.createConnection(
 );
 
 
-inquirer.promt({
-    type: 'list',
-    message: "Choose:",
-    name: "query",
-    choices:[
-        "View all departments",
-        "View all roles",
-        "View allemployees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee role"
-    ],
-    validate: (input) => {
-        return input ? true : (console.log(this.message), false)
+async function promptUser(){
+    await inquirer.prompt([
+        {
+        type: 'list',
+        message: "Choose:",
+        name: "query",
+        choices:[
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role"
+        ]}
+    ]).then((data) => {
+        switch (data.query){
+            case "View all departments":
+                viewDepartments();
+                break;
+            case "View all roles":
+                viewRoles();
+                break;
+            case "View all employees":
+                viewEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add a role":
+                addRole();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update an employee role":
+                updateEmployee();
+                break;
+        }
+    });
+}
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
     }
 });
+
+
+promptUser()
+
+function viewDepartments(){
+
+}
+function viewRoles(){
+
+}
+function viewEmployees(){
+
+}
+function addDepartment(){
+
+}
+function addRole(){
+
+}
+function addEmployee(){
+
+}
+function updateEmployee(){
+
+}
+
 
